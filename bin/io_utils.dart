06fda96ec3417147a4a7e067a246cd3654665833
ln-sergey <lnov.sergey@gmail.com>
2021-03-05 23:39:33 +0300

    * Add method arrayFromJosn

--- bin/io_utils.dart
+++ bin/io_utils.dart
@@ -7,6 +7,26 @@ import 'dart:async';
 /// And writing arrays to .json files
 abstract class IOUtils {
 
+  /// ### Reads array from _filePath_
+  /// 
+  /// * If file type isn't a json then throws [Exception]
+  /// * If file contains no [int] array inside 'data' part of json
+  /// then throws [Exception]
+  static Future<List<int>> arrayFromJson(String filePath) async {
+    if (!_validateIfJson(filePath)) throw Exception();
+
+    // Read json file as String
+    // Then take an array from 'data' part of jsonMap 
+    final fileContent = await File(filePath).readAsString();
+    final array = json.decode(fileContent)['data'];
+    
+    if (array == null) {
+      throw Exception();
+    } else {
+      return array;
+    }
+  }
+
   /// ### Returns if file type is _.json_
   static bool _validateIfJson(String filePath) {
     try {
