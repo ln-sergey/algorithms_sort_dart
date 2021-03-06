959436c6a9299cb493e19b14470a84cf7e6e0aa9
ln_sergey <lnov.sergey@gmail.com>
2021-03-06 03:15:47 +0300

    * Create unit-test for bubble_sort.dart

--- test/algorithms/bubble_sort_test.dart
+++ test/algorithms/bubble_sort_test.dart
@@ -1,7 +1,24 @@
 import 'package:test/test.dart';
+import '../../bin/algorithms/bubble_sort.dart';
 
 void main() {
   test('bubble_sort', () {
-    expect(true, true);
+    // arrange
+    final tArray1 = <int>[9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    final tArray2 = <int>[21, 75, 12, 4, 7, 79, 30, 11, 12, 75];
+    final tArray3 = <int>[5];
+    final tArray4 = <int>[];
+
+    // act
+    bubbleSortImpl(tArray1);
+    bubbleSortImpl(tArray2);
+    bubbleSortImpl(tArray3);
+    bubbleSortImpl(tArray4);
+
+    // assert
+    expect(tArray1, equals([...tArray1]..sort()));
+    expect(tArray2, equals([...tArray2]..sort()));
+    expect(tArray3, equals([...tArray3]..sort()));
+    expect(tArray4, equals([...tArray4]..sort()));
   });
 }
